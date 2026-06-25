09861e07def4219e8b69b54b4994c9a87a9bb86b
Joe Allen <jhallenworld@gmail.com>
2026-06-25 02:07:35 -0400

    More stuff

---
+++ basic/ram_sp_async.v
@@ -0,0 +1,25 @@
+// Parameterized single-port inferred RAM
+// This RAM has async read port: Altera has no such RAM
+
+module ram_sp_async
+#(
+  parameter DATAWIDTH = 8,
+  parameter ADDRWIDTH = 9
+) (
+  input reset_l,
+  input clk,
+  input [DATAWIDTH-1:0] wr_data,
+  input [ADDRWIDTH-1:0] addr,
+  input we,
+  output wire [DATAWIDTH-1:0] rd_data
+  );
+
+reg [DATAWIDTH-1:0] ram[((1 << ADDRWIDTH) - 1) : 0];
+
+always @(posedge clk)
+  if (we)
+    ram[addr] <= wr_data;
+
+assign rd_data = ram[addr];
+
+endmodule
---
+++ basic/reqack.v
@@ -0,0 +1,35 @@
+// Make flip request / flip ack sequence look like a FIFO access
+
+module reqack
+  (
+  input reset_l,
+  input clk,
+  input req,		// Flip for request
+  output reg ack,	// Flip for ack
+  output reg ne,	// (not empty) A request is pending
+  input re		// (read enable) Pulse to acknowledge request
+  );
+
+wire req_synced;
+
+simple_sync req_syncer
+  (
+  .reset_l (reset_l),
+  .clk (clk),
+  .i (req),
+  .o (req_synced)
+  );
+
+always @(posedge clk or negedge reset_l)
+  if (!reset_l)
+    begin
+      ack <= 0;
+      ne <= 0;
+    end
+  else
+    begin
+      ne <= req_synced != ack;
+      if (re) ack <= !ack;
+    end
+
+endmodule
